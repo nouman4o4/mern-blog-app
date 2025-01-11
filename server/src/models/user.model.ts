6451ddff72e53f1464dcc8b6c00187b08d9adec7
Nouman Khan <nomiofficial68@gmail.com>
2025-01-11 20:41:40 +0500

    userModel, and modification of password

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,5 +1,5 @@
-import { Schema, Types } from "mongoose";
-
+import { Schema, Types, model } from "mongoose";
+import bcrypt from "bcrypt";
 interface IUser {
   firstname: string;
   lastname: string;
@@ -8,4 +8,36 @@ interface IUser {
   posts: Types.ObjectId;
 }
 
-const userSchema = new Schema({}, { timestamps: true });
+const userSchema = new Schema(
+  {
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    posts: [{ type: Types.ObjectId, ref: "Post" }],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (error: any) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model<IUser>("User", userSchema);
+
+export { User, IUser };
