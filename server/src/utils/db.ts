d44016948b0873289e6c0a8616f623ffed8be1ea
Nouman Khan <nomiofficial68@gmail.com>
2025-01-19 22:54:37 +0500

    fixed db connection

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -3,9 +3,9 @@ import mongoose, { connection } from "mongoose";
 export default async function connectDb() {
   const url = "mongodb://localhost:27017/";
   try {
-    await mongoose.connect("mongodb://localhost:27017/mernBlogApp");
+    await mongoose.connect("mongodb://127.0.0.1:27017/mernBlogApp");
     // const host = connectionInstance.connection.host;
-    console.log(`Databse connected, DB HOST:`);
+    console.log(`Database successfully connected, DB HOST:`);
   } catch (error) {
     console.log("MongoDb connection failed, error: ", error);
     process.exit(1);
