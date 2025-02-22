f35e0e32ef75df3a384f364e499dad055f7c523b
Nouman Khan <nomiofficial68@gmail.com>
2025-02-22 15:28:59 +0500

    added the express.json() middelware

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,13 +5,14 @@ import { configDotenv } from "dotenv";
 
 const app = express();
 const port = 3000;
-
 configDotenv();
-app.use("/api/users", authRouter);
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.use("/api/users", authRouter);
 
 app.listen(port, async () => {
   await connectDb();
