5beaf88f7bae6cfeee90467087a54394013d3bca
Nouman Khan <nomiofficial68@gmail.com>
2025-03-02 22:44:46 +0500

    cors setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,19 @@ import connectDb from "./utils/db";
 import authRouter from "./routes/auth-route";
 import { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 configDotenv();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
