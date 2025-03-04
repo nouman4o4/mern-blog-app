5009f1ca52c35a513cfc7b960543ce94fecf75a3
Nouman Khan <nomiofficial68@gmail.com>
2025-03-04 22:45:42 +0500

    some improvements in auth-controller

--- server/src/controllers/auth-controller.ts
+++ server/src/controllers/auth-controller.ts
@@ -1,23 +1,18 @@
-import express, {
-  Request,
-  RequestHandler,
-  Response,
-  response,
-} from "express";
+import express, { Request, Response } from "express";
 import { User, IUser } from "../models/user.model";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
+// Register
 export const register = async (req: Request, res: Response) => {
   const {
     firstname,
     lastname,
     email,
     password,
-
     gender,
-  }: Partial<IUser> = await req.body;
-  const confirmPassword = await req.body.confirmPassword;
+  }: Partial<IUser> = req.body;
+  const confirmPassword = req.body.confirmPassword;
 
   if (
     [
@@ -27,27 +22,32 @@ export const register = async (req: Request, res: Response) => {
       password,
       confirmPassword,
       gender,
-    ].some((elem) => elem?.trim() === "")
+    ].some((elem) => !elem?.trim())
   ) {
-    return res
-      .status(400)
-      .json({ message: "Please fill in all fields" });
+    return res.status(400).json({
+      success: false,
+      status: 400,
+      message: "Please fill in all fields",
+    });
   }
   if (password !== confirmPassword) {
-    return res
-      .status(400)
-      .json({ message: "Passwords do not match" });
+    return res.status(400).json({
+      success: false,
+      status: 400,
+      message: "Passwords do not match",
+    });
   }
-  // find existting user in the db
+
   try {
-    const existtingUser = await User.findOne({ email });
-    if (existtingUser) {
-      return res
-        .status(404)
-        .json({ message: "Email is in already in use" });
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({
+        success: false,
+        status: 409,
+        message: "Email is already in use",
+      });
     }
 
-    // create  a new user
     const newUser = await User.create({
       firstname,
       lastname,
@@ -55,45 +55,52 @@ export const register = async (req: Request, res: Response) => {
       password,
       gender,
     });
-    if (!newUser) {
-      throw Error;
-    }
+    if (!newUser) throw new Error("User creation failed");
+
     return res.status(201).json({
+      success: true,
+      status: 201,
       message: "User created successfully",
-      user: {
-        firstname: newUser.firstname,
-        lastname: newUser.lastname,
-        email: newUser.email,
-      },
+      data: { firstname, lastname, email },
     });
   } catch (error) {
-    console.log(error);
-    return res
-      .status(400)
-      .json({ message: "unexpected error occured: ", error });
+    return res.status(500).json({
+      success: false,
+      status: 500,
+      message: "Unexpected error occurred",
+      error,
+    });
   }
 };
 
+// login
 export const login = async (req: Request, res: Response) => {
   const { email, password }: Partial<IUser> = req.body;
   if (!email || !password) {
-    return res
-      .status(400)
-      .json({ message: "Please fill in all fields" });
+    return res.status(400).json({
+      success: false,
+      status: 400,
+      message: "Please fill in all fields",
+    });
   }
 
   try {
-    // find user
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({
+        success: false,
+        status: 404,
+        message: "User not found",
+      });
     }
-    // check password
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-      return res.status(401).json({ message: "Invalid password" });
+      return res.status(401).json({
+        success: false,
+        status: 401,
+        message: "Invalid password",
+      });
     }
-    // generate token
     const token = jwt.sign(
       { userId: user._id },
       process.env.SECRET_KEY as string,
@@ -106,109 +113,133 @@ export const login = async (req: Request, res: Response) => {
       sameSite: "strict",
     });
     return res.status(200).json({
+      success: true,
+      status: 200,
       message: "Logged in successfully",
-      user: {
+      data: {
         firstname: user.firstname,
         lastname: user.lastname,
         email: user.email,
       },
     });
   } catch (error) {
-    return res
-      .status(400)
-      .json({ message: "unexpected error occured: ", error });
-  }
-};
-
-// log out
-
-export const logout = (req: Request, res: Response) => {
-  try {
-    res.cookie("token", "", {
-      maxAge: 0,
-    });
-    return res
-      .status(200)
-      .json({ message: "user loged out successfully!" });
-  } catch (error) {
-    return res.status(400).json({
-      message: "unexpected error occured while loging out",
+    return res.status(500).json({
+      success: false,
+      status: 500,
+      message: "Unexpected error occurred",
       error,
     });
   }
 };
 
-// updating a user
+// logout
+export const logout = (req: Request, res: Response) => {
+  try {
+    res.cookie("token", "", { maxAge: 0 });
+    return res.status(200).json({
+      success: true,
+      status: 200,
+      message: "User logged out successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      status: 500,
+      message: "Unexpected error occurred while logging out",
+      error,
+    });
+  }
+};
+
+// updateUser
 export const updateUser = async (req: Request, res: Response) => {
   const userId = req.userId;
   const { firstname, lastname }: Partial<IUser> = req.body;
 
   if (!firstname && !lastname) {
-    return res.status(400).json({ message: "No updates provided" });
+    return res.status(400).json({
+      success: false,
+      status: 400,
+      message: "No updates provided",
+    });
   }
   try {
-    const updateFields: Partial<IUser> = {};
-
-    if (firstname) updateFields.firstname = firstname;
-    if (lastname) updateFields.lastname = lastname;
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: userId },
-      { $set: updateFields },
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: { firstname, lastname } },
       { new: true }
     );
     if (!updatedUser) {
-      return res
-        .status(404)
-        .json({ message: "User not found to udpate" });
+      return res.status(404).json({
+        success: false,
+        status: 404,
+        message: "User not found to update",
+      });
     }
     return res.status(200).json({
-      message: "User updated succesffully",
-      user: {
-        id: updatedUser._id,
-        firstname: updatedUser.firstname,
-        lastname: updatedUser.lastname,
-        email: updatedUser.email,
-      },
+      success: true,
+      status: 200,
+      message: "User updated successfully",
+      data: updatedUser,
     });
   } catch (error) {
-    return res.status(400).json({
-      message: "an unexpected error uccured while udpating the user",
+    return res.status(500).json({
+      success: false,
+      status: 500,
+      message: "Unexpected error occurred while updating the user",
       error,
     });
   }
 };
+
+// delte a user
 export const removeUser = async (req: Request, res: Response) => {
   const userId = req.params.id;
-  // check if the userId is available and correct
   try {
-    if (!userId || mongoose.isValidObjectId(userId)) {
-      return res.status(400).json({ message: "invalid user id" });
+    if (!mongoose.isValidObjectId(userId)) {
+      return res.status(400).json({
+        success: false,
+        status: 400,
+        message: "Invalid user ID",
+      });
     }
-    const deletedUser = User.findByIdAndDelete(userId);
+    const deletedUser = await User.findByIdAndDelete(userId);
     if (!deletedUser) {
-      return res.status(400).json({ message: "user not found" });
+      return res.status(404).json({
+        success: false,
+        status: 404,
+        message: "User not found",
+      });
     }
-
-    return res
-      .status(204)
-      .json({ message: "User deleted successfully" });
+    return res.status(200).json({
+      success: true,
+      status: 200,
+      message: "User deleted successfully",
+    });
   } catch (error) {
-    return res.status(400).json({
+    return res.status(500).json({
+      success: false,
+      status: 500,
       message: "Unexpected error occurred while deleting user",
       error,
     });
   }
 };
 
-// get all the users
+// get all users
 export const getUsers = async (req: Request, res: Response) => {
   try {
     const users = await User.find();
-    return res
-      .status(200)
-      .json({ message: "Operation successfull!", data: users });
+    return res.status(200).json({
+      success: true,
+      status: 200,
+      message: "Operation successful",
+      data: users,
+    });
   } catch (error) {
-    return res.status(400).json({
+    return res.status(500).json({
+      success: false,
+      status: 500,
       message: "Unexpected error occurred while fetching users",
       error,
     });
