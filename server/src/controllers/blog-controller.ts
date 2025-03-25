5e5d937101c06e96602008b40c77b8dc107e0e43
Nouman Khan <nomiofficial68@gmail.com>
2025-03-25 22:57:35 +0500

    create blog api endpoint

--- server/src/controllers/blog-controller.ts
+++ server/src/controllers/blog-controller.ts
@@ -1,8 +1,65 @@
 import { Request, Response } from "express";
+import { uploadFileToCloudinary } from "../lib/cloundinary";
+import { Post } from "../models/posts.model";
 
 // Create blog post
 export const createBlog = async (req: Request, res: Response) => {
-  res.json({ message: "Create a blog" });
+  const { title, content, author, attachments, category } = req.body;
+  if (!title || !content || !author || category) {
+    res.status(400).json({
+      success: false,
+      status: 400,
+      message: "Please provide all the required fields",
+    });
+    return;
+  }
+  const featuredImageFile = req.file;
+  let featuredImageUrl: string | undefined = "";
+  try {
+    const imageUploadResult = await uploadFileToCloudinary(
+      featuredImageFile?.path as string
+    );
+    featuredImageUrl = imageUploadResult?.secure_url;
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      status: 500,
+      message: "Image upload failed",
+      error,
+    });
+  }
+
+  try {
+    const newPost = await Post.create({
+      title,
+      content,
+      author,
+      category,
+      featuredImageUrl,
+      attachments,
+    });
+    if (!newPost) {
+      res.status(500).json({
+        success: false,
+        status: 500,
+        message: "Something went wrong while create a new blog post",
+      });
+      return;
+    }
+
+    res.status(201).json({
+      success: true,
+      status: 201,
+      message: "New blog post created successfully!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      status: 500,
+      message: "Something went wrong while create a new blog post",
+      error,
+    });
+  }
 };
 // Get all blog posts
 export const getAllBlogs = async (req: Request, res: Response) => {
