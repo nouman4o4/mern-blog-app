124b64599a042815b288b5bf1ce97b428662a6fa
Nouman Khan <nomiofficial68@gmail.com>
2025-03-27 22:02:02 +0500

    fixed an issue in coudinary function

---
+++ server/src/lib/cloundinary.ts
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+
+import "dotenv/config";
+
+export const uploadFileToCloudinary = async function (
+  localFilePath: string
+) {
+  if (!localFilePath) return;
+
+  // Configuration
+  cloudinary.config({
+    cloud_name: "dsvnh6opu",
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+  // Upload an image
+  try {
+    const uploadResult = await cloudinary.uploader.upload(
+      localFilePath,
+      {
+        public_id: "blogImage",
+        resource_type: "auto",
+      }
+    );
+
+    return uploadResult;
+  } catch (error) {
+    console.log(
+      "Error while uploading image to the cloudinary:",
+      error
+    );
+  }
+};
