c83f12c31d1d41c1fff34de134365d4410226360
Nouman Khan <nomiofficial68@gmail.com>
2025-04-25 22:43:28 +0500

    utillity function for profile image upload

---
+++ client/src/utils/uploadImage.ts
@@ -0,0 +1,12 @@
+export async function uploadProfileImage(userId: string, file: Blob) {
+  const url = `http://localhost:3000/api/v1/users/update-profile/${userId}`;
+  const formData = new FormData();
+  formData.append("profile", file, `${userId}_profile.jpg`);
+  const response = await fetch(url, {
+    method: "PUT",
+    body: formData,
+    credentials: "include",
+  });
+  const jsonReponse = await response.json();
+  return jsonReponse;
+}
