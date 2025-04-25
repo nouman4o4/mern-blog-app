c82f5c0fff36ff59c3358778d8c220eebccf636b
Nouman Khan <nomiofficial68@gmail.com>
2025-04-25 22:40:27 +0500

    CropModal, ImageCropper and UserDetailSection, components for the profile page

---
+++ client/src/components/CropModal.tsx
@@ -0,0 +1,166 @@
+import React from "react";
+import useImageCropper from "../hooks/useImageCropper";
+import { X } from "lucide-react";
+import { uploadProfileImage } from "../utils/uploadImage";
+import toast from "react-hot-toast";
+import useUserStore from "../store/userStore";
+import { useNavigate, useNavigation } from "react-router";
+import ReactCrop from "react-image-crop";
+type PropsTypes = {
+  imageSrc: string;
+  setImageSrc: React.Dispatch<React.SetStateAction<string>>;
+  setIsUploading: React.Dispatch<React.SetStateAction<boolean>>;
+};
+export default function CropModal({
+  imageSrc,
+  setImageSrc,
+  setIsUploading,
+}: PropsTypes) {
+  const { authUser: user, setAuthUser } = useUserStore();
+  const navigate = useNavigate();
+  const {
+    crop,
+    setCrop,
+    completedCrop,
+    setCompletedCrop,
+    croppedUrl,
+    setCroppedUrl,
+    isCropped,
+    setIsCropped,
+    imgRef,
+    imageFile,
+    setImageFile,
+    getCroppedImage,
+  } = useImageCropper();
+
+  const setImageAndUpload = async () => {
+    toast("Image is uploading...");
+    if (!imageFile) {
+      toast.error("Image file not found");
+      return;
+    }
+    try {
+      setIsUploading(true);
+      setImageSrc("");
+
+      const jsonResponse = await uploadProfileImage(
+        user?._id as string,
+        imageFile
+      );
+
+      if (!jsonResponse.success) {
+        console.log(jsonResponse);
+
+        if (jsonResponse.message === "User not found") {
+          localStorage.removeItem("blog-app-user");
+          setAuthUser(null);
+          toast.error("Something went wrong, Please login again.");
+
+          setIsCropped(false);
+          setImageSrc("");
+          setCroppedUrl("");
+          navigate("/login");
+          return;
+        }
+        toast.error(
+          jsonResponse.message ||
+            "Something went wrong, Please login again."
+        );
+        setIsCropped(false);
+        setImageSrc("");
+        setCroppedUrl("");
+        return;
+      }
+      const userData = jsonResponse.data;
+      setAuthUser({
+        firstname: userData.firstname,
+        lastname: userData.lastname,
+        email: userData.email,
+        gender: userData.gender,
+        profileImage: userData.profileImage,
+        _id: userData._id,
+      });
+      toast.success("Profile updated successfully!");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  const onImageLoaded = (img: HTMLImageElement) => {
+    imgRef.current = img;
+  };
+
+  if (!imageSrc) return;
+  return (
+    <div className="profile-image-preview absolute top-0 left-0 z-20 h-screen w-screen bg-black/75 flex items-center justify-center p-2">
+      <div className="bg-white rounded-lg w-full md:w-8/12 h-fit max-h-3/4 p-5 flex items-center flex-col relative xlg:pt-4 pt-14">
+        <div
+          onClick={() => {
+            setImageSrc("");
+            setCroppedUrl("");
+            setImageFile("");
+          }}
+          className="absolute top-2 right-2 text-xl cursor-pointer text-red-500 border-1 border-black bg-white rounded-full p-[1px]">
+          <X className="size-4 md:size-6" />
+        </div>
+
+        {isCropped ? (
+          <div className="h-full flex justify-center flex-col gap-10">
+            <div className={`image w-44 h-44 sm:w-64 sm:h-64 `}>
+              <img
+                onClick={() => {
+                  setImageFile("");
+                  setCroppedUrl("");
+                  setIsCropped(false);
+                }}
+                src={croppedUrl}
+                alt=""
+                className="w-full h-full cursor-pointer object-cover rounded-full border-black border-2 "
+              />
+            </div>
+            <div className="btns text-center">
+              <button
+                onClick={() => {
+                  setImageFile("");
+                  setCroppedUrl("");
+                  setIsCropped(false);
+                }}
+                className="mt-4 px-4 py-2 bg-gray-200 hover:bg-gray-300 text-black shadow rounded">
+                Edit
+              </button>
+              <button
+                onClick={setImageAndUpload}
+                className="mt-4 mx-2 px-4 py-2 bg-red-400 hover:bg-red-500 text-white rounded">
+                Set And Upload
+              </button>
+            </div>
+          </div>
+        ) : (
+          <>
+            <ReactCrop
+              className="h-fit max-h-[400px] md:max-h-[500px] mx-auto w-fit max-w-9/10 overflow-hidden"
+              crop={crop}
+              onChange={(c) => setCrop(c)}
+              onComplete={(c) => setCompletedCrop(c)}
+              aspect={1 / 1}>
+              <img
+                src={imageSrc}
+                ref={imgRef}
+                onLoad={(e) => onImageLoaded(e.currentTarget)}
+                alt="To Crop"
+                className="w-full h-full object-contain"
+              />
+            </ReactCrop>
+            <button
+              onClick={getCroppedImage}
+              className="mt-4 px-4 py-2 bg-gray-900 hover:scale-105 text-white rounded">
+              Crop Image
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ client/src/components/UserDetailSection.tsx
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from "react";
+import useImageCropper from "../hooks/useImageCropper";
+import useUserStore from "../store/userStore";
+import { Camera, Loader } from "lucide-react";
+import CropModal from "./CropModal";
+
+export default function UserDetailSection() {
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [imageSrc, setImageSrc] = useState<string>("");
+  const { croppedUrl, setCroppedUrl, isCropped, setIsCropped } =
+    useImageCropper();
+  const { authUser, setAuthUser } = useUserStore();
+
+  useEffect(() => {
+    if (imageSrc) {
+      document.body.style.height = "100vh";
+      document.body.style.overflowY = "hidden";
+    } else if (!imageSrc) {
+      document.body.style.height = "auto";
+      document.body.style.overflowY = "auto";
+    }
+  }, [isCropped, imageSrc]);
+
+  const handleFileChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      if (typeof reader.result === "string") {
+        setImageSrc(reader.result);
+        setIsCropped(false); // Reset if cropping again
+        setCroppedUrl("");
+      }
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const getProfileImage = () => {
+    if (isCropped && croppedUrl && isUploading) return croppedUrl;
+    if (authUser?.profileImage) return authUser?.profileImage;
+    return authUser?.gender === "male"
+      ? "https://avatar.iran.liara.run/public/41"
+      : "https://avatar.iran.liara.run/public/88";
+  };
+
+  return (
+    <>
+      <CropModal
+        imageSrc={imageSrc}
+        setImageSrc={setImageSrc}
+        setIsUploading={setIsUploading}
+      />
+
+      <div className="min-h-[70vh] w-full bg-white rounded-2xl">
+        <div className="user-profile&details w-full h-40 sm:h-56 md:h-74 rounded-2xl bg-center bg-cover bg-[url(https://images.unsplash.com/photo-1508780709619-79562169bc64?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]"></div>
+
+        <div className="w-full flex flex-col sm:flex-row min-h-[30vh] justify-between">
+          <div className="user-image">
+            <div className="photo w-32 h-32 sm:w-48 sm:h-48 absolute sm:relative">
+              <div className="absolute bg-white left-4 top-[-40%] border-2 border-black rounded-full">
+                <img
+                  src={getProfileImage()}
+                  alt=""
+                  className={`w-full h-full object-cover rounded-full ${
+                    isUploading ? "opacity-60" : ""
+                  }`}
+                />
+                {isUploading && (
+                  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold text-white">
+                    <Loader />
+                  </div>
+                )}
+                <div className="absolute right-2 bottom-2 bg-red-500 rounded-full p-2 text-lg text-white">
+                  <label htmlFor="profile-image">
+                    <Camera className=" " />
+                    <input
+                      id="profile-image"
+                      name="profile-image"
+                      type="file"
+                      accept=".png,.jpg,.jpeg"
+                      onChange={handleFileChange}
+                      hidden
+                    />
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex-grow p-6 pt-18 sm:pt-6">
+            <h1 className="text-4xl font-bold text-gray-800 mb-3">
+              {authUser
+                ? authUser?.firstname + " " + authUser?.lastname
+                : "loading..."}
+            </h1>
+            <div className="flex flex-wrap">
+              <div className="user-detail sm:w-1/2">
+                <p className="font-semibold text-gray-800 mb-4">
+                  Email:{" "}
+                  <span className="text-gray-500">
+                    {authUser ? authUser.email : "loading..."}
+                  </span>
+                </p>
+                <p className="font-semibold text-gray-800 mb-4">
+                  Phone:{" "}
+                  <span className="text-gray-500">
+                    +1 (555) 123-4567
+                  </span>
+                </p>
+                <p className="text text-gray-800 mb-4 font-semibold">
+                  Gender:{" "}
+                  <span className="text-gray-500">Female</span>
+                </p>
+              </div>
+              <div className="w-full sm:w-1/2 sm:px-5">
+                <p className="font-semibold text-gray-800 mb-4">
+                  Posts: <span className="text-gray-500">15</span>
+                </p>
+                <p className="font-semibold text-gray-800 mb-4">
+                  Total likes:{" "}
+                  <span className="text-gray-500">35</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
