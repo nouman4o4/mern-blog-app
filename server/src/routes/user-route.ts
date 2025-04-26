6f819dba6af3841ba798cff6f56670b4e5d2b051
Nouman Khan <nomiofficial68@gmail.com>
2025-04-26 16:40:38 +0500

    added comments for readablity

--- server/src/routes/user-route.ts
+++ server/src/routes/user-route.ts
@@ -11,10 +11,15 @@ import { upload } from "../middleswares/multer";
 
 const userRouter = Router();
 
+// Get all users
 userRouter.get("/", authMiddleware, getUsers);
+// Get a user
 userRouter.get("/:id", authMiddleware, getUser);
+// Delete a user
 userRouter.get("/delete/:id", removeUser);
+// Update a user
 userRouter.put("/update/:id", authMiddleware, updateUser);
+// Update profile image
 userRouter.put(
   "/update-profile/:id",
   upload.single("profile"),
