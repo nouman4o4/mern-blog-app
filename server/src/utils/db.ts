a4f4c71934c85f68ffb2c75f9a63641f136fea21
Nouman Khan <nomiofficial68@gmail.com>
2025-08-22 23:09:59 +0500

    feat: update database connection to use environment variable for MongoDB URI

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -1,9 +1,9 @@
 import mongoose, { connection } from "mongoose";
 
 export default async function connectDb() {
-  const url = "mongodb://localhost:27017/";
+  const url = process.env.MONGODB_URI as string;
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/mernBlogApp");
+    await mongoose.connect(url);
     // const host = connectionInstance.connection.host;
     console.log(`Database successfully connected, DB HOST:`);
   } catch (error) {
