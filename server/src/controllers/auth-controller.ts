d57a9fbc4615556d34787a2fd667af4fb7f6e5f9
Nouman Khan <nomiofficial68@gmail.com>
2025-11-16 22:57:43 +0500

    fix auth contoller returns to fix typscript warning

--- server/src/controllers/auth-controller.ts
+++ server/src/controllers/auth-controller.ts
@@ -1,51 +1,44 @@
-import express, { Request, Response } from "express";
-import { User, IUser } from "../models/user.model";
-import jwt from "jsonwebtoken";
-import mongoose from "mongoose";
+import express, { Request, Response } from "express"
+import { User, IUser } from "../models/user.model"
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
 
 // Register
 export const register = async (req: Request, res: Response) => {
-  const {
-    firstname,
-    lastname,
-    email,
-    password,
-    gender,
-  }: Partial<IUser> = req.body;
-  const confirmPassword = req.body.confirmPassword;
+  const { firstname, lastname, email, password, gender }: Partial<IUser> =
+    req.body
+  const confirmPassword = req.body.confirmPassword
 
   if (
-    [
-      firstname,
-      lastname,
-      email,
-      password,
-      confirmPassword,
-      gender,
-    ].some((elem) => !elem?.trim())
+    [firstname, lastname, email, password, confirmPassword, gender].some(
+      (elem) => !elem?.trim()
+    )
   ) {
-    return res.status(400).json({
+    res.status(400).json({
       success: false,
       status: 400,
       message: "Please fill in all fields",
-    });
+    })
+    return
   }
   if (password !== confirmPassword) {
-    return res.status(400).json({
+    res.status(400).json({
       success: false,
       status: 400,
       message: "Passwords do not match",
-    });
+    })
+    return
   }
 
   try {
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email })
     if (existingUser) {
-      return res.status(409).json({
+      res.status(409).json({
         success: false,
         status: 409,
         message: "Email is already in use",
-      });
+      })
+      return
     }
 
     const newUser = await User.create({
@@ -54,10 +47,10 @@ export const register = async (req: Request, res: Response) => {
       email,
       password,
       gender,
-    });
-    if (!newUser) throw new Error("User creation failed");
+    })
+    if (!newUser) throw new Error("User creation failed")
 
-    return res.status(201).json({
+    res.status(201).json({
       success: true,
       status: 201,
       message: "User created successfully",
@@ -67,57 +60,62 @@ export const register = async (req: Request, res: Response) => {
         email: newUser.email,
         _id: newUser._id,
       },
-    });
+    })
+    return
   } catch (error) {
-    return res.status(500).json({
+    res.status(500).json({
       success: false,
       status: 500,
       message: "Unexpected error occurred",
       error,
-    });
+    })
+    return
   }
-};
+}
 
 // login
 export const login = async (req: Request, res: Response) => {
-  const { email, password }: Partial<IUser> = req.body;
+  const { email, password }: Partial<IUser> = req.body
   if (!email || !password) {
-    return res.status(400).json({
+    res.status(400).json({
       success: false,
       status: 400,
       message: "Please fill in all fields",
-    });
+    })
+    return
   }
 
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email })
     if (!user) {
-      return res.status(404).json({
+      res.status(404).json({
         success: false,
         status: 404,
         message: "User not found",
-      });
+      })
+      return
     }
-    const isMatch = await user.comparePassword(password);
+    const isMatch = await user.comparePassword(password)
     if (!isMatch) {
-      return res.status(401).json({
+      res.status(401).json({
         success: false,
         status: 401,
         message: "Invalid password",
-      });
+      })
+      return
     }
     const token = jwt.sign(
       { userId: user._id },
       process.env.SECRET_KEY as string,
       { expiresIn: "15d" }
-    );
+    )
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 24 * 60 * 60 * 1000,
       secure: process.env.NODE_ENV !== "development",
       sameSite: "strict",
-    });
-    return res.status(200).json({
+    })
+    res.status(200).json({
       success: true,
       status: 200,
       message: "Logged in successfully",
@@ -129,42 +127,46 @@ export const login = async (req: Request, res: Response) => {
         profileImage: user.profileImage,
         _id: user._id,
       },
-    });
+    })
+    return
   } catch (error) {
-    return res.status(500).json({
+    res.status(500).json({
       success: false,
       status: 500,
       message: "Unexpected error occurred",
       error,
-    });
+    })
+    return
   }
-};
+}
 
 // logout
 export const logout = (req: Request, res: Response) => {
   try {
-    res.cookie("token", "", { maxAge: 0 });
-    return res.status(200).json({
+    res.cookie("token", "", { maxAge: 0 })
+    res.status(200).json({
       success: true,
       status: 200,
       message: "User logged out successfully",
-    });
+    })
+    return
   } catch (error) {
-    return res.status(500).json({
+    res.status(500).json({
       success: false,
       status: 500,
       message: "Unexpected error occurred while logging out",
       error,
-    });
+    })
+    return
   }
-};
+}
 
 // verify auth
 
 export const verifyAuth = async (req: Request, res: Response) => {
   try {
-    const userId = req.userId;
-    const requestedUserId = req.params.id;
+    const userId = req.userId
+    const requestedUserId = req.params.id
 
     if (
       !mongoose.isValidObjectId(requestedUserId) ||
@@ -174,35 +176,33 @@ export const verifyAuth = async (req: Request, res: Response) => {
         success: false,
         status: 400,
         message: "Incorrect user id",
-      });
-      return;
+      })
+      return
     }
 
-    const authUser = await User.findById(requestedUserId).select(
-      "-password"
-    );
+    const authUser = await User.findById(requestedUserId).select("-password")
     if (!authUser) {
       res.status(404).json({
         status: 404,
         success: false,
         message: "User not found",
-      });
-      return;
+      })
+      return
     }
 
     res.status(200).json({
       success: true,
       status: 200,
       message: "User is verified",
-    });
-    return;
+    })
+    return
   } catch (error: any) {
     res.status(500).json({
       status: 500,
       success: false,
       message: "Something went wrong while verifying the user",
       error: error.stack,
-    });
-    return;
+    })
+    return
   }
-};
+}
--- server/src/routes/auth-route.ts
+++ server/src/routes/auth-route.ts
@@ -1,19 +1,19 @@
-import express, { RequestHandler, Request, Response } from "express";
+import express, { RequestHandler, Request, Response, Router } from "express"
 import {
   login,
   logout,
   register,
   verifyAuth,
-} from "../controllers/auth-controller";
-import { authMiddleware } from "../middleswares/authMiddleware";
+} from "../controllers/auth-controller"
+import { authMiddleware } from "../middleswares/authMiddleware"
 
-const authRouter = express.Router();
+const authRouter = Router()
 
 // typescript error occurring becuase of returning the response;
 
-authRouter.post("/register", register);
-authRouter.post("/login", login);
-authRouter.get("/logout/:id", logout);
-authRouter.get("/verify/:id", authMiddleware, verifyAuth);
+authRouter.post("/register", register)
+authRouter.post("/login", login)
+authRouter.get("/logout/:id", logout)
+authRouter.get("/verify/:id", authMiddleware, verifyAuth)
 
-export default authRouter;
+export default authRouter
