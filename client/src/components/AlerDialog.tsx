658fb57a972c1fdc06c6bdb3640730bbb2b07761
Nouman Khan <nomiofficial68@gmail.com>
2025-11-17 21:24:13 +0500

    fix: remove unused imported hook

--- client/src/components/AlerDialog.tsx
+++ client/src/components/AlerDialog.tsx
@@ -1,44 +1,41 @@
-import React from "react";
-import toast from "react-hot-toast";
-import { useNavigate } from "react-router";
+import React from "react"
+import toast from "react-hot-toast"
 
 export default function AlerDialog({
   setIsAlert,
   blogId,
   setRefreshQuery,
 }: {
-  setIsAlert: React.Dispatch<React.SetStateAction<boolean>>;
-  blogId: string;
-  setRefreshQuery: () => void;
+  setIsAlert: React.Dispatch<React.SetStateAction<boolean>>
+  blogId: string
+  setRefreshQuery: () => void
 }) {
   // handle blog delete
   const handleDelete = async () => {
     try {
-      const url = `${
-        import.meta.env.VITE_BASE_SERVER_URL
-      }/blogs/${blogId}`;
+      const url = `${import.meta.env.VITE_BASE_SERVER_URL}/blogs/${blogId}`
       const response = await fetch(url, {
         method: "DELETE",
         credentials: "include",
-      });
+      })
       if (!response.ok) {
-        console.log(response);
-        toast.error("Failed to delete, try again!");
+        console.log(response)
+        toast.error("Failed to delete, try again!")
       }
-      const jsonResponse = await response.json();
+      const jsonResponse = await response.json()
       if (!jsonResponse.success) {
-        toast.error("Fialed to delete, try again!");
-        console.log(jsonResponse.message);
+        toast.error("Fialed to delete, try again!")
+        console.log(jsonResponse.message)
       }
-      toast.success("Blog post deleted successfully!");
-      setIsAlert(false);
-      setRefreshQuery();
+      toast.success("Blog post deleted successfully!")
+      setIsAlert(false)
+      setRefreshQuery()
     } catch (error) {
-      setIsAlert(false);
-      console.log(error);
-      toast.error("Something went, couldn't delete");
+      setIsAlert(false)
+      console.log(error)
+      toast.error("Something went, couldn't delete")
     }
-  };
+  }
 
   return (
     <div className="fixed inset-0 bg-black/45">
@@ -49,17 +46,19 @@ export default function AlerDialog({
           <div className="text-center">
             <button
               onClick={() => setIsAlert(false)}
-              className="w-2/5 bg-black text-white py-2 rounded-lg mr-3 cursor-pointer hover:scale-105 transition-all duration-100">
+              className="w-2/5 bg-black text-white py-2 rounded-lg mr-3 cursor-pointer hover:scale-105 transition-all duration-100"
+            >
               Cancel
             </button>
             <button
               onClick={handleDelete}
-              className="w-2/5 bg-red-400 text-white py-2 rounded-lg ml-3 cursor-pointer hover:scale-105 transition-all duration-100">
+              className="w-2/5 bg-red-400 text-white py-2 rounded-lg ml-3 cursor-pointer hover:scale-105 transition-all duration-100"
+            >
               Delete
             </button>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
